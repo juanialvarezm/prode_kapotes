d8a8faba7429f1602116e0a3b5447c2bdae82923
Juani <juanialvarezzm@gmail.com>
2026-04-22 21:01:04 -0300

    changed migrations env

--- back/migrations/env.py
+++ back/migrations/env.py
@@ -36,14 +36,7 @@ def get_engine_url():
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-import os
-
-db_url = os.getenv("DATABASE_URL")
-if db_url and db_url.startswith("mysql://"):
-    db_url = db_url.replace("mysql://", "mysql+pymysql://", 1)
-
-config.set_main_option('sqlalchemy.url', db_url)
-
+config.set_main_option('sqlalchemy.url', get_engine_url())
 target_db = current_app.extensions['migrate'].db
 
 # other values from the config, defined by the needs of env.py,
